c9c2aef238e7c16c9b4ecd34cdd7ac6f4bf39ca5
derrick-maebar <derrickorori@gmail.com>
2026-01-21 09:47:17 +0300

    updated and created the file while demo.php

---
+++ WhileDemo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>While Demo</title>
+</head>
+<body>
+    <h1>While Demo</h1>
+<?php
+$n=1;
+while($n<=5)
+{
+    echo "$n <br/>";
+    $n++;
+}
+?>
+</body>
+</html>
