c9826516f5725a1cc422da78f243a6f394105a03
derrick-maebar <derrickorori@gmail.com>
2026-02-01 15:14:16 +0300

    updated and added the following files,arrays and foreeachloop

---
+++ arrays.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+         //indexed arrays
+$array =  array("Derrick","Ian","angel","Morgan","Doris");
+           echo $array[4];
+        //associative arrays
+$data = array("Derrick"=>"First","Morgan"=>"second","age"=>"30");
+        //multidimensional array
+$data = array(array("Faith","Daniesl"),"john","jane");
+    ?>
+</body>
+</html>
---
+++ foreachloop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $array = array("Morgan","Ian","angel");
+    foreach ($array as $loopdata) {
+        echo "my name is". $loopdata."<br>";
+    }
+    ?>
+</body>
+</html>
